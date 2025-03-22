e0637c4fd9362607bd56687f27797af3a7e72950
James Yang <jamesyang663@gmail.com>
2025-03-22 13:27:48 -0400

    sms webhook

--- server/test.js
+++ server/test.js
@@ -4,8 +4,6 @@ const accountSid = process.env.TWILIO_ACCOUNTSID
 const authToken = process.env.TWILIO_AUTHTOKEN
 
 console.log(accountSid, authToken)
-// const accountSid = 'ACd9a2eb80667dae60865d078aedd9e815';
-// const authToken = '14a36c5a9cb8312754509600648ad6cb';
 
 const client = require('twilio')(accountSid, authToken);
 
